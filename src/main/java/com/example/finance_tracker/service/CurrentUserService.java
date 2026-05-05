7ffe70656be3f1ad29963096c13e6e467b1b08c7
nikita <tnik538@gmail.com>
2026-05-05 14:32:51 +0300

    добавил интерфейс для CurrentUserService

--- src/main/java/com/example/finance_tracker/service/CurrentUserService.java
+++ src/main/java/com/example/finance_tracker/service/CurrentUserService.java
@@ -1,19 +1,8 @@
 package com.example.finance_tracker.service;
 
-import com.example.finance_tracker.exception.UnauthorizedException;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Service;
 
-@Service
-public class CurrentUserService {
+public interface CurrentUserService {
 
-    public Long getCurrentUserId(HttpSession session) {
-        Object userId = session.getAttribute("userId");
-
-        if (userId == null) {
-            throw new UnauthorizedException("Пользователь не авторизован");
-        }
-
-        return (Long) userId;
-    }
+    Long getCurrentUserId(HttpSession session);
 }
